3569375d280f96ac9086fb656550b8b78cf0be00
NorigH29 <huelvan@lacroixrouge-brest.fr>
2019-12-18 17:05:25 +0100

    Version2

--- roquette.py
+++ roquette.py
@@ -7,6 +7,15 @@ class Roquette(object):
         self.x=x
         self.y=y
         self.r=self.parent.create_oval(self.x-5,self.y-5,self.x+5,self.y+5,fill=coul)
+
+
+    def tir(self,angle,power):
+        t=0
+        x=cos(angle*pi/180)*power*t
+        y=-1/2*9.81*pow(t,2)+sin(angle*pi/180)*power*t
+        self.parent.move
+
+
     def destroy(self):
         print("destruction dela roquette")
         self.parent.delete(self.r)
