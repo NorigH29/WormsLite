f2afb36535af79960ea7f3d5938a9094f0fde68d
NorigH29 <huelvan@lacroixrouge-brest.fr>
2019-12-18 16:51:53 +0100

    Version4

--- wormsLite.py
+++ wormsLite.py
@@ -2,39 +2,46 @@ from tkinter import *
 from bazooka import *
 from roquette import *
 
-def create_roquette1():
-    global roquette1
-    roquette1=Roquette(can,b1.x2,b1.y2,'red')
-
-def destroy_roquette1():
-    roquette1.destroy()
+class Worms(Frame):
 
 
-# Code pour tester sommairement la classe Bazouka :
-f = Tk()
-can = Canvas(f,width =500, height =250, bg ='ivory')
-can.pack(padx =10, pady =10)
-b1 = Bazooka(can, 50, 200)
-b2 = Bazooka(can, 250, 200)
+    def create_roquette1(self):
+        self.roquette1
+        self.roquette1=Roquette(self.can,self.b1.x2,self.b1.y2,'red')
 
-s1 =Scale(f, label='hausse', from_=90, to=0, command=b1.orienter)
-s1.pack(side=LEFT, pady =5, padx =20)
-s1.set(25)                          # angle de tir initial
+    def destroy_roquette1(self):
+        self.roquette1.destroy()
 
-s2 =Scale(f, label='hausse', from_=90, to=0, command=b2.orienter)
-s2.pack(side=LEFT, pady =5, padx =20)
-s2.set(25)
+    def __init__(self):
 
-roquette1= Roquette(can,125,150,'red')
+        # Code pour tester sommairement la classe Bazouka :
+        f = Tk()
+        self.can = Canvas(f,width =500, height =250, bg ='ivory')
+        self.can.pack(padx =10, pady =10)
+        self.b1 = Bazooka(self.can, 50, 200)
+        self.b2 = Bazooka(self.can, 250, 200)
+
+        s1 =Scale(f, label='hausse', from_=90, to=0, command=self.b1.orienter)
+        s1.pack(side=LEFT, pady =5, padx =20)
+        s1.set(25)                          # angle de tir initial
+
+        s2 =Scale(f, label='hausse', from_=90, to=0, command=self.b2.orienter)
+        s2.pack(side=LEFT, pady =5, padx =20)
+        s2.set(25)
+
+        self.roquette1= Roquette(self.can,125,150,'red')
 
 
-btn_tir1 = Button(f,text="tire",command=create_roquette1)
-btn_tir1.pack(side=LEFT,pady =14,padx =20)
+        btn_tir1 = Button(f,text="tire",command=self.create_roquette1)
+        btn_tir1.pack(side=LEFT,pady =14,padx =20)
 
-btn_tir2 = Button(f,text="tire",command=create_roquette1)
-btn_tir2.pack(side=RIGHT,pady =14,padx =20)
+        btn_tir2 = Button(f,text="tire",command=self.create_roquette1)
+        btn_tir2.pack(side=RIGHT,pady =14,padx =20)
 
-btn_supr = Button(f,text="suprimer",command=destroy_roquette1)
-btn_supr.pack(side=BOTTOM,pady =14,padx =20)
+        btn_supr = Button(f,text="suprimer",command=self.destroy_roquette1)
+        btn_supr.pack(side=BOTTOM,pady =14,padx =20)
 
-f.mainloop()
+        f.mainloop()
+
+
+monJeux=Worms()
